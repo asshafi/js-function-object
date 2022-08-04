48c95f4da69e7fddb67b1f4284a9c82e1372784a
S M SHAFI <asshafi15@gmail.com>
2022-08-04 13:32:39 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,9 @@
+// function declration 
+function startFan() {
+    console.log('Stand Up');
+    console.log('Walk toward the switch');
+    console.log('press the switch');
+}
+
+// call the function
+startFan();
---
+++ parameter.js
@@ -0,0 +1,15 @@
+function bringSingara(money) {
+    console.log('mama singara dao');
+    console.log('eto tk disen:',money);
+}
+// bringSingara(100);
+function add(num1, num2) {
+    console.log("going to add:", num1, num2);
+}
+// add(50, 1500);
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(1,3,5,2,5);
