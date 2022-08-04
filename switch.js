bf7af6a5eaceb2c2028864c58ee742356a5c14d3
S M SHAFI <asshafi15@gmail.com>
2022-08-04 17:45:47 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,39 @@
+// if else condition
+// const color = 'yellow';
+// if (color === 'green'){
+   
+// }
+// else if (color === 'blue'){
+//     console.log('Your are a blue friend');
+// }
+// else if (color === 'red'){
+//     console.log('Your are a red friend');
+// }
+// else if (color === 'white'){
+//     console.log('Your are a white friend');
+// }
+// else if (color === 'yellow'){
+//     console.log('Your are a yellow friend');
+// }
+// else {
+//     console.log('Your are a black friend');
+// }
+
+// switch condition 
+const color = 'yellow';
+switch (color) {
+    case 'green':
+        console.log('Your are a green friend');
+        break;
+    case 'blue':
+        console.log('Your are a blue friend');
+        break;
+    case 'yellow':
+        console.log('Your are a yellow friend');
+        break;
+    case 'white':
+        console.log('Your are a white friend');
+        break;
+    default:
+        console.log('You are a black firend');
+}
