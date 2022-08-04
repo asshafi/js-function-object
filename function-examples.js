85a00845fbf6af7b321eb9b70c842f73a217f298
S M SHAFI <asshafi15@gmail.com>
2022-08-04 16:24:00 +0600

    third commit

---
+++ function-examples.js
@@ -0,0 +1,16 @@
+// function functionName(parameter) {
+//     function body
+//         return
+// }
+// var returnedValu = functionName(parameter value)
+function getAverage(assignment1, assignment2, assignment3) {
+    const total = assignment1 + assignment2 + assignment3;
+    const average = total / 3;
+    return average;
+}
+const assignment1Marks = 57;
+const assignment2Marks = 60;
+const assignment3Marks = 60;
+
+var myAverage = getAverage(assignment1Marks, assignment2Marks, assignment3Marks);
+console.log('My average so far:', myAverage);
--- return.js
+++ return.js
@@ -1,6 +1,17 @@
-function add(number1, number2) {
-    console.log(number1, number2);
-    var sum = number1 + number2;
-    console.log(sum);
+// function add(number1, number2) {
+//     console.log(number1, number2);
+//     var sum = number1 + number2;
+//     // console.log(sum);
+//     return sum;
+// }
+// // add(34, 23);
+// var total = add(23, 43);
+// // console.log('total', total);
+function bringSingara(money) {
+    var singaraPrice = 10;
+    var quantity = money / singaraPrice;
+    return quantity;
 }
-add(34, 23);
+var myTaka = 150;
+var singaras = bringSingara(myTaka);
+console.log('Eating singara', singaras);
